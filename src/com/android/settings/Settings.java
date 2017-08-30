6cfaad78a41bd4b8bc213a5d5c316564f8c84655
maxwen <max.weninger@gmail.com>
2017-08-31 00:30:09 +0200

    Let's Add Shore dashboard entry
    xlxfoxxlx: update to P, add oval background
    
    Change-Id: Iaf927e8fef982681e6d485ea4e878b6c988a6596
    Signed-off-by: Josh Fox(XlxFoXxlX) <joshfox87@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -165,4 +165,7 @@ public class Settings extends SettingsActivity {
     public static class AccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
 
+    // Shore
+    public static class ShoreSettingsActivity extends SettingsActivity {}
+
 }
